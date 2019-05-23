5807b2400c935d696d052d17abb28d3ae103c521
Muqiu7890 <1337651840@qq.com>
2019-05-24 01:46:11 +0800

    finish lcd

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+module.exports = function main(num) {
+    let topPart = ['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.']
+    let centerPart = ['|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|']
+    let bottomPart = ['|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|']
+    let topRes = []
+    let centerRes = []
+    let bottomRes = []
+    num.split('').map(num => {
+        topRes.push(topPart[Number(num)])
+        centerRes.push(centerPart[Number(num)])
+        bottomRes.push(bottomPart[Number(num)])
+    })
+    return `${topRes.join(' ')}\n${centerRes.join(' ')}\n${bottomRes.join(' ')}\n`;
+}
